467b032b9e96ff6e30325bde205da4dcd950102d
TranPhamMinhDuc <doggygerman@gmail.com>
2025-06-17 21:34:05 +0700

    Add files via upload

---
+++ End,Die,Save/Over.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Restart : MonoBehaviour
+{
+    void Start(){
+        Time.timeScale = 1f;
+        
+
+    }
+    public void RestartGame()
+    {
+        
+        SceneManager.LoadScene("Scene1");
+    }
+
+    public void ExitGame()
+    {
+        
+    #if UNITY_EDITOR
+            UnityEditor.EditorApplication.isPlaying = false;
+    #else
+            Application.Quit(); // Dành cho build thật
+    #endif
+    }
+}
