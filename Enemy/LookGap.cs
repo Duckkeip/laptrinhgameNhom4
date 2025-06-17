521c1f04e1a607e36d42adbc360532d941dc603c
TranPhamMinhDuc <doggygerman@gmail.com>
2025-06-17 21:30:33 +0700

    Add files via upload
    scipt enemy

---
+++ Enemy/LookGap.cs
@@ -0,0 +1,21 @@
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+public class LookGap: MonoBehaviour
+{
+    public GameObject obstacleObject;
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Ground") || collision.gameObject.CompareTag("Player") || collision.gameObject.CompareTag("Enemy"))
+        {
+        obstacleObject.transform.localScale = new Vector3(-obstacleObject.transform.localScale.x, obstacleObject.transform.localScale.y,
+         obstacleObject.transform.localScale.z);
+        }
+    }
+}
+
+
+
+
+
+
